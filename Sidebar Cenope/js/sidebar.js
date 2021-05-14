3f16306a445029e1d6c0c7ae1dabd43918fe6e6f
FrancoNavarro20 <frankitonv@gmail.com>
2021-05-14 13:06:08 -0300

    Sidebar cenope

--- Sidebar Cenope/js/sidebar.js
+++ Sidebar Cenope/js/sidebar.js
@@ -1,6 +1,6 @@
 'use strict'
 
-
+$("#sub-menu-search2").hide();  // Esta linea oculta el sub menu 2
 var search = document.querySelector("#search");
 var bandera = 1;
 search.addEventListener('click', () =>{
@@ -20,10 +20,24 @@ search.addEventListener('click', () =>{
 
     //Para aparecer y desaparecer el sidebar-search
     document.querySelector(".sidebar-search").classList.toggle('active');
+    //document.querySelector("#sub-menu-search2").classList.toggle('active2');
 });
 
+$(document).ready(function () {
 
+    $("#covid-19").click(function (e) { 
+        $("#sub-menu-search").hide();
+        $("#sub-menu-search2").show(); //Esta linea vuelve a mostrar el submenu2
+    });
+     
+});
 
-
+$("#crossicon").click(function (e) { 
+    $("#sub-menu-search2").hide();   
+    $("#sub-menu-search").show(); 
+});
+$("#search").click(function (e) { 
+    $("#sub-menu-search2").hide();
+});
 
 
