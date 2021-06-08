60bcd5e8bf1ad9c1928a8d443198d4fc97b07f55
eiberti <eiberti@gmail.com>
2021-06-08 11:34:44 -0300

    Cambios MY Iberti

--- Sidebar Cenope/database.php
+++ Sidebar Cenope/database.php
@@ -5,9 +5,12 @@ require 'conexion_database.php';
 $consulta = "SELECT u.nodo,u.localidad,c.* FROM capa_sig_unidades_militares c
 INNER JOIN unidad u ON u.cod_unidad=c.cod_uni";
 
+//$_POST['cod_uni']="U2055";
+
 if (isset($_POST['cod_uni']) && $_POST['cod_uni']!="TODOS") {
-    $consulta .= " WHERE c.cod_unisup='".$_POST['cod_uni']."'";
+    $consulta .= " WHERE (c.cod_uni='".$_POST['cod_uni']."' or c.cod_unisup='".$_POST['cod_uni']."') AND c.cod_uni like 'U%' order by c.cod_uni desc";
 }
+//echo $consulta;
 
 $resultado = mysqli_query( $conexion, $consulta ) or die ( "Algo ha ido mal en la consulta a la base de datos");
 
