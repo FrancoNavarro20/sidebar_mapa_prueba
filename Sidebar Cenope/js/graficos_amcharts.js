adbd9aa457df3075054ce589357450c6ce662385
eiberti <eiberti@gmail.com>
2021-06-16 09:38:21 -0300

    cambio de estilo en graficos

--- Sidebar Cenope/js/graficos_amcharts.js
+++ Sidebar Cenope/js/graficos_amcharts.js
@@ -39,56 +39,55 @@ function cargarEstadisticasPersonal() {
 }
 
 function chart_personal(div_chart,titulo,datos_json) {
+  // Themes begin
+  am4core.useTheme(am4themes_material);
+  am4core.useTheme(am4themes_animated);
+  // Themes end
 
-// Themes begin
-am4core.useTheme(am4themes_dataviz);
-am4core.useTheme(am4themes_animated);
-// Themes end
+  // Create chart instance
+  var chart = am4core.create(div_chart, am4charts.XYChart);
 
-// Create chart instance
-var chart = am4core.create(div_chart, am4charts.XYChart);
+  // Title
+  var title = chart.titles.push(new am4core.Label());
+  title.text = titulo;
+  title.fontSize = 20;
+  title.marginTop = 15;
+  title.marginBottom = 10;
 
-// Title
-var title = chart.titles.push(new am4core.Label());
-title.text = titulo;
-title.fontSize = 20;
-title.marginTop = 15;
-title.marginBottom = 10;
+  // Add data
+  chart.data = datos_json;
 
-// Add data
-chart.data = datos_json;
+  // Create axes
 
-// Create axes
+  var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+  categoryAxis.dataFields.category = "category";
+  categoryAxis.renderer.grid.template.location = 0;
+  categoryAxis.renderer.minGridDistance = 30;
 
-var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
-categoryAxis.dataFields.category = "category";
-categoryAxis.renderer.grid.template.location = 0;
-categoryAxis.renderer.minGridDistance = 30;
+  categoryAxis.renderer.labels.template.adapter.add("dy", function(dy, target) {
+    if (target.dataItem && target.dataItem.index & 2 == 2) {
+      return dy + 15;
+    }
+    return dy;
+  });
 
-categoryAxis.renderer.labels.template.adapter.add("dy", function(dy, target) {
-  if (target.dataItem && target.dataItem.index & 2 == 2) {
-    return dy + 25;
-  }
-  return dy;
-});
+  var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+  valueAxis.extraMax = 0.11;
 
-var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
-valueAxis.extraMax = 0.1;
+  // Create series
+  var series = chart.series.push(new am4charts.ColumnSeries());
+  series.dataFields.valueY = "first";
+  series.dataFields.categoryX = "category";
+  series.name = "Cant personal";
+  series.columns.template.tooltipText = "{categoryX}: [bold]{valueY}[/]";
+  series.columns.template.fillOpacity = .8;
 
-// Create series
-var series = chart.series.push(new am4charts.ColumnSeries());
-series.dataFields.valueY = "first";
-series.dataFields.categoryX = "category";
-series.name = "Cant personal";
-series.columns.template.tooltipText = "{categoryX}: [bold]{valueY}[/]";
-series.columns.template.fillOpacity = .8;
+  var columnTemplate = series.columns.template;
+  columnTemplate.strokeWidth = 2;
+  columnTemplate.strokeOpacity = 1;
 
-var columnTemplate = series.columns.template;
-columnTemplate.strokeWidth = 2;
-columnTemplate.strokeOpacity = 1;
-
-var labelBullet = series.bullets.push(new am4charts.LabelBullet());
-labelBullet.label.verticalCenter = "bottom";
-labelBullet.label.dy = -10;
-labelBullet.label.text = "{values.valueY.workingValue.formatNumber('#.')}";
+  var labelBullet = series.bullets.push(new am4charts.LabelBullet());
+  labelBullet.label.verticalCenter = "bottom";
+  labelBullet.label.dy = -10;
+  labelBullet.label.text = "{values.valueY.workingValue.formatNumber('#.')}";
 }
