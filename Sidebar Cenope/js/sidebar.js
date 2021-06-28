4610671d3f0a7a8af9f0dac2a00a1a575938e58a
FrancoNavarro20 <frankitonv@gmail.com>
2021-06-28 02:36:07 -0300

    cambios de tamaño de fuente cant personal

--- Sidebar Cenope/js/sidebar.js
+++ Sidebar Cenope/js/sidebar.js
@@ -35,6 +35,7 @@ var uni_agrupadas = L.markerClusterGroup.layerSupport();
     });
   });
   
+
   $(".elem_conducc_sup").hide();
 
   mostrar_marcadores("TODOS");
@@ -379,40 +380,3 @@ $(".hover_areas ul").on("click", "li", function () {
 //     map.invalidateSize();
 //   }, 1000);
 // }
-
-//Botones de los graficos
-$(".pulsar_elemt_grafic").click(function () {
-  //alert("Anda los botones");
-  
-  var origen = $(this);
-  var universidad = origen.data("grafic");
-  switch (universidad) {
-    case "datos":
-      //alert("Alerta de datos");
-      $(".elemt_graficos_mil").hide('slow');
-      $(".elemt_datos").show('slow');
-      $("#boton_datos").addClass("elemt_pulsado_grafic");
-      $("#boton_datos a").addClass("elemt_pulsado_grafic_a");
-      $("#boton_grafic a").removeClass("elemt_pulsado_grafic_a");
-      $("#boton_grafic").removeClass("elemt_pulsado_grafic");
-      break;
-    case "graficos":
-      //alert("Alerta de graficos");
-      $(".elemt_datos").hide('slow');
-      $(".elemt_graficos_mil").show('slow');
-      $("#boton_grafic").addClass("elemt_pulsado_grafic");
-      $("#boton_datos").removeClass("elemt_pulsado_grafic");
-      $("#boton_datos a").removeClass("elemt_pulsado_grafic_a");
-      $("#boton_grafic a").addClass("elemt_pulsado_grafic_a");
-      break;
-    default:
-      alert(
-        "Hubo un error, no se selecciono una universidad pública o privada ."
-      );
-      break;
-  }
-  
-});
-
-
-
